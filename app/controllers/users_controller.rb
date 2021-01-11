e7cac47bdc003015feceb91524e019c575e9fbc6
pablo malczewski <malczewskipablo@gmail.com>
2021-01-10 22:31:09 -0300

    add params to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,20 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
 
 
-  def index
-  @users = User.all
-  end
-  
   def show
     @user = User.find(params[:id])
+  end
+
+
+private
+
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :username)
+  end
+
+  def set_user
+    @user = current_user
   end
 end
